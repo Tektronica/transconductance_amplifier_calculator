719661c8b39afd1e10e7378ecbf608b2a0ca7558
Tektronica <9472267+Tektronica@users.noreply.github.com>
2021-02-09 20:23:48 -0800

    Streamlined functionality

--- TCA_calculator.py
+++ TCA_calculator.py
@@ -1,58 +1,34 @@
+import math
+
 # TODO -  LCAPY for circuit rendering?
 
-
-# def TCA_0(Vsupply, Vin, Gain, Rset, Ioffset, Ra, R2, beta=100):
-#     # Single Class-A TCA
-#     Vpeak = Vin * 1.4149
-#     Iout_max = Vpeak * Gain + Ioffset
-#
-#     Vemitter_max = Vsupply - Ioffset * ((beta + 1) / beta) * Rset
-#     Vemitter_min = Vsupply - (Iout_max * ((beta + 1) / beta)) * Rset
-#     Vemitter_swing = Vemitter_max - Vemitter_min
-#
-#     Rb = Ra * (Vemitter_min / (Vsupply - Vemitter_min))
-#
-#     gain_input = ((Vemitter_swing / Vpeak) * (1 + Rb / Ra))
-#     R1 = R2 * (gain_input - 1)
-#
-#     return R1, Rb
-
-
-def TCA_1(Vsupply, Vsignal, transconductance, Rset, Iquiescent, Ra, R2, beta=100):
+"""
+def TCA_2(Vsupply, Vin, Gain, Rset, Ioffset, Ra, R2, beta=100):
     # Balanced Complementary Class-A TCA
-    Vpeak = Vsignal * 1.4149
-    Ipeak = Vpeak * transconductance
+    Vpeak = Vin * 1.4149
+    Iout_max = Vpeak * Gain  # A
 
-    a = beta / (beta + 1)
-    Imid = (Ipeak/2)/a
+    Vemitter_max = Vsupply - Ioffset * ((beta + 1) / beta) * Rset
+    Vemitter_min = Vsupply - (Iout_max * ((beta + 1) / beta) + Ioffset) * Rset
+    Vemitter_swing = Vemitter_max - Vemitter_min
+    Vemitter_mid = Vemitter_max - Vemitter_swing / 2
 
-    Vin = Imid * Rset
-    Voffset = Vsupply - Rset * (Imid + Iquiescent/a)
+    Rb = Ra * (Vemitter_mid / (Vsupply - Vemitter_mid))
 
-    Rb = Ra * (Vsupply / (Vsupply - Voffset)-1)
-
-    gain = ((Vin + Voffset) * (Ra + Rb) - Vsupply * Rb) / (Ra * Vpeak)
-    R1 = R2 * (gain - 1)
+    gain_input = ((Vemitter_swing / (2 * Vpeak)) * (1 + Rb / Ra))
+    R1 = R2 * (gain_input - 1)
 
     return R1, Rb
+"""
 
 
-# def TCA_2(Vsupply, Vin, Gain, Rset, Ioffset, Ra, R2, beta=100):
-#     # Balanced Complementary Class-A TCA
-#     Vpeak = Vin * 1.4149
-#     Iout_max = Vpeak * Gain  # A
-#
-#     Vemitter_max = Vsupply - Ioffset * ((beta + 1) / beta) * Rset
-#     Vemitter_min = Vsupply - (Iout_max * ((beta + 1) / beta) + Ioffset) * Rset
-#     Vemitter_swing = Vemitter_max - Vemitter_min
-#     Vemitter_mid = Vemitter_max - Vemitter_swing / 2
-#
-#     Rb = Ra * (Vemitter_mid / (Vsupply - Vemitter_mid))
-#
-#     gain_input = ((Vemitter_swing / (2 * Vpeak)) * (1 + Rb / Ra))
-#     R1 = R2 * (gain_input - 1)
-#
-#     return R1, Rb
+def print_dictionary(d):
+    for k, v in d.items():
+        print(k)
+        print('------------------------------------')
+        for _k, _v in v.items():
+            print(f'{_k}: {_v}')
+        print('')
 
 
 def TCA_circuit():
@@ -62,51 +38,59 @@ def TCA_circuit():
     pass
 
 
-def main():
-    # Single Class-A TCA -----------------------------------------------------------------------------------------------
-    # Vsupply = 10
-    # Vin = 1
-    # Gain = 1  # A/V
-    # Rset = 0.4
-    # Ioffset = 0.01  # A
-    # beta = 100
-    #
-    # R2 = 10000
-    # Ra = 1000
-    #
-    # R1, Rb = TCA_0(Vsupply, Vin, Gain, Rset, Ioffset, Ra, R2, beta)
-    # print('Single Class-A TCA')
-    # print(f'R1: {round(R1, 2)}\tR2: {round(R2, 2)}\nRa: {round(Ra, 2)}\tRb: {round(Rb, 2)}')
+# TODO - Simplified model
+def TCA_1(Vsupply, Vinput, transconductance, Rset, Iquiescent, Ra=1000, R2=10000, beta=100):
+    # Balanced Complementary Class-A TCA
+    Vpeak = Vinput * 1.4149
+    Ipeak = Vpeak * transconductance
 
+    a = beta / (beta + 1)
+    Imid = (Ipeak / 2) / a
+
+    Vin = Imid * Rset
+    Voffset = Vsupply - Rset * (Imid + Iquiescent / a)
+
+    Rb = Ra * (Vsupply / (Vsupply - Voffset) - 1)
+
+    gain = ((Vin + Voffset) * (Ra + Rb) - Vsupply * Rb) / (Ra * Vpeak)
+    R1 = R2 * (gain - 1)
+
+    return {'R1': round(R1, 2), 'Rb': round(Rb, 2)}
+
+
+def get_1A3A_Values(Vsupply, Rset, Ra=1000, R2=10000, beta=100):
+    HighTCA_dict = {'1.2A': TCA_1(Vsupply=Vsupply, Vinput=6, transconductance=0.2,
+                                  Rset=Rset, Iquiescent=0.150, Ra=Ra, R2=R2, beta=beta),
+
+                    '3.1A': TCA_1(Vsupply=Vsupply, Vinput=3.1, transconductance=1,
+                                  Rset=Rset, Iquiescent=0.1, Ra=Ra, R2=R2, beta=beta)}
+    print_dictionary(HighTCA_dict)
+
+    print('------------------------------------')
+    # the value used in parallel with the 3.1A range resistor when in the 1.2A range
+    R11 = HighTCA_dict['1.2A']['R1']
+    R13 = HighTCA_dict['3.1A']['R1']
+    try:
+        R1_parallel = round(1 / (1 / R11 - 1 / R13), 2)
+    except ZeroDivisionError:
+        print(f'No parallel value needed for R1')
+    else:
+        print(f'parallel R1: {R1_parallel} for 1.2A')
+
+    # the value used in parallel with the 1.2A range resistor when in the 3.1A range
+    Rb1 = HighTCA_dict['1.2A']['Rb']
+    Rb3 = HighTCA_dict['3.1A']['Rb']
+    Rb_parallel = round(1 / (1 / Rb3 - 1 / Rb1), 2)
+    print(f'parallel Rb: {Rb_parallel} for 3.1A')
+    print('------------------------------------')
+
+
+def main():
     # Balanced Complementary Class-A TCA -------------------------------------------------------------------------------
     Vsupply = 10
-    Vsignal = 3.1
-    transconductance = 1  # A/V
-    Rset = 0.200
-    Iquiescent = 0.100  # A
-    beta = 100
+    Rset = 0.2
 
-    R2 = 10000
-    Ra = 1000
-
-    R1, Rb = TCA_1(Vsupply, Vsignal, transconductance, Rset, Iquiescent, Ra, R2, beta)
-    print('\nBalanced Complementary Class-A TCA')
-    print(f'R1: {round(R1, 2)}\tR2: {round(R2, 2)}\nRa: {round(Ra, 2)}\tRb: {round(Rb, 2)}')
-
-    # Balanced Complementary Class-A TCA -------------------------------------------------------------------------------
-    # Vsupply = 10
-    # Vin = 6
-    # Gain = 0.2  # A/V
-    # Rset = 0.2
-    # Ioffset = 0.100  # A
-    # beta = 100
-    #
-    # R2 = 10000
-    # Ra = 1000
-    #
-    # R1, Rb = TCA_2(Vsupply, Vin, Gain, Rset, Ioffset, Ra, R2, beta)
-    # print('\nBalanced Complementary Class-A TCA')
-    # print(f'R1: {round(R1, 2)}\tR2: {round(R2, 2)}\nRa: {round(Ra, 2)}\tRb: {round(Rb, 2)}')
+    get_1A3A_Values(Vsupply, Rset)
 
 
 if __name__ == '__main__':
